97d34ec8e09c8c62259f7e8c22492c4e28a43786
Ethan Nguyen <sevadus98@gmail.com>
2023-05-25 23:02:42 -0700

    Updated to NextJS 13.4.4 which is currently incompatible with next-auth

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        swcPlugins: [
-            ['next-superjson-plugin', {}] // allows us to safely pass datetime objects and other properties that might not be compatible from server to client
-        ]
+        // swcPlugins: [
+        //     ['next-superjson-plugin', {}] // allows us to safely pass datetime objects and other properties that might not be compatible from server to client
+        // ],
+        // serverActions: true, // allows us to use server side actions
     },
     images: {
         domains: [
@@ -14,4 +15,4 @@ const nextConfig = {
     }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
