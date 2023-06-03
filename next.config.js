09a1bd2d7d6887ead013e2e056b7c1a31792c9d6
Ethan Nguyen <sevadus98@gmail.com>
2023-06-03 04:04:58 -0700

    Installed next-cloudinary package

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        // swcPlugins: [
-        //     ['next-superjson-plugin', {}] // allows us to safely pass datetime objects and other properties that might not be compatible from server to client
-        // ],
-        // serverActions: true, // allows us to use server side actions
-    },
-    images: {
-        domains: [
-            'res.cloundinary.com',
-            'avatars.githubusercontent.com',
-            'lh3.googleusercontent.com'
-        ]
-    }
-}
+  experimental: {
+    // swcPlugins: [
+    //     ['next-superjson-plugin', {}] // allows us to safely pass datetime objects and other properties that might not be compatible from server to client
+    // ],
+    // serverActions: true, // allows us to use server side actions
+  },
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+};
 
 module.exports = nextConfig;
